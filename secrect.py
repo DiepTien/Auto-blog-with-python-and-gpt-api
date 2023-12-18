ff4fdf156f1744b40c22c4593406223432e2092c
DiepTien <dmatttqd@gmail.com>
2023-12-18 12:53:22 +0700

    Create secrect.py

---
+++ secrect.py
@@ -0,0 +1,13 @@
+open_ai_key = "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"
+wordpress_api_url = 'https://artisticalcove.com/'
+wordpress_username = 'dmatttqd'
+wordpress_password = 'wLut LTRG pXkY BQLl AuWZ wBcR'
+hugging_face_headers = {"Authorization": "Bearer hf_odmaXbmbLJaTuJCVWrdKZfZFiE"}
+gpt_model = "gpt-3.5-turbo-1106"
+gpt_model_4 = "gpt-4"
+dalle_model ="dall-e-3"
+gpt_temperature = 0
+art_keywords = [
+    "logistics"
+]
+keyword = "Những Lưu Ý Quan Trọng Khi Chọn Dịch Vụ Giao Hàng Đường Biển Đi Mỹ"
